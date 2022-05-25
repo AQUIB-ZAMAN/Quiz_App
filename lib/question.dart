6bc40a4512f904731691419043db0816cb6af062
AQUIB-ZAMAN <64305732+AQUIB-ZAMAN@users.noreply.github.com>
2022-05-26 02:57:02 +0530

    updated main.dart file and creating a new custom widget for the questions

---
+++ lib/question.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// creating a new custom widget
+class Question extends StatelessWidget {
+  String text;
+  Question({this.text: ""}) {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
